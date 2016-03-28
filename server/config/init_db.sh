b2b082fc7e505b7e63772cb070ea69d0c3313749
Romain Zanchi <romainzanchi30@gmail.com>
2016-03-28 21:35:14 +0200

    fix init db

--- server/config/init_db.sh
+++ server/config/init_db.sh
@@ -2,4 +2,4 @@
 
 while ! nc -vz 'mysql' $MYSQL_PORT_3306_TCP_PORT; do sleep 1; done
 
-mysql -u root -p$RSSSERVER_RSS_MYSQL_1_ENV_MYSQL_ROOT_PASSWORD -h 'mysql' < /root/server/src/main/resources/rss.sql
+mysql -u root -proot -h 'mysql' < /root/server/src/main/resources/rss.sql
