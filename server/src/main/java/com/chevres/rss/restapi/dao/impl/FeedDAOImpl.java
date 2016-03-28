72598fd336211c8ecea36ca3dacdc7b1ebff5e0e
wayath <anthony@a-lingua.fr>
2016-03-28 23:17:03 +0200

    adding Feed entity, dao, bean

---
+++ server/src/main/java/com/chevres/rss/restapi/dao/impl/FeedDAOImpl.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chevres.rss.restapi.dao.impl;
+
+import com.chevres.rss.restapi.dao.FeedDAO;
+
+/**
+ *
+ * @author anthony
+ */
+public class FeedDAOImpl extends AbstractGenericDAO implements FeedDAO {
+    
+}
---
+++ server/src/main/java/com/chevres/rss/restapi/model/Feed.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chevres.rss.restapi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author anthony
+ */
+@Entity
+@Table(name = "feed")
+public class Feed {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "url")
+    private String url;
+    @Column(name = "id_user")
+    private int idUser;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+}
