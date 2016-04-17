49f2e90c3538d2263e2c05824b14bc94438546e4
Romain Zanchi <romainzanchi30@gmail.com>
2016-04-17 14:24:17 +0200

    login required to refresh a feed

--- server/src/main/java/com/chevres/rss/worker/controller/WorkerController.java
+++ server/src/main/java/com/chevres/rss/worker/controller/WorkerController.java
@@ -7,12 +7,18 @@ package com.chevres.rss.worker.controller;
 
 import com.chevres.rss.restapi.controller.jsonresponse.ErrorMessageResponse;
 import com.chevres.rss.restapi.controller.jsonresponse.SuccessMessageResponse;
+import com.chevres.rss.restapi.dao.FeedDAO;
+import com.chevres.rss.restapi.dao.UserAuthDAO;
+import com.chevres.rss.restapi.model.Feed;
+import com.chevres.rss.restapi.model.UserAuth;
 import com.chevres.rss.worker.feedupdater.FeedUpdater;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,9 +31,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class WorkerController {
     @RequestMapping(path = "/worker/refresh/feed/{feedId}", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<String> refreshFeed(@PathVariable int feedId) {
+    public ResponseEntity<String> refreshFeed(
+            @RequestHeader(value = "User-token") String userToken,
+            @PathVariable int feedId) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        FeedDAO feedDAO = context.getBean(FeedDAO.class);
+        UserAuthDAO userAuthDAO = context.getBean(UserAuthDAO.class);
+        
+        UserAuth userAuth = userAuthDAO.findByToken(userToken);
+        if (userAuth == null) {
+            return new ResponseEntity(new ErrorMessageResponse("invalid_token"),
+                    HttpStatus.BAD_REQUEST);
+        }
+        Feed feed = feedDAO.findById(userAuth, feedId);
+        if (feed == null) {
+            return new ResponseEntity(new ErrorMessageResponse("bad_params"),
+                    HttpStatus.BAD_REQUEST);
+        }
+        context.close();
         FeedUpdater feedUpdater = new FeedUpdater();
-        if (feedUpdater.updateFeedById(feedId)) {
+        if (feedUpdater.updateFeed(feed)) {
             return new ResponseEntity(new SuccessMessageResponse("success"),
                 HttpStatus.OK);
         }
